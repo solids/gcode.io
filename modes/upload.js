cffd9182b20d0482d5e8b93bcb6dd23e9192e06d
Elijah Insua <tmpvar@gmail.com>
2014-03-06 14:05:38 -0800

    allow model to be posted via postMessage

--- modes/upload.js
+++ modes/upload.js
@@ -1,10 +1,35 @@
 var dropHandler = require('drop-stl-to-json');
 var tools = require('editor3-meshtools');
 
+var postMessageImport = require('../lib/importFromPostMessage');
+
 function UploadMode(modeManager, element) {
   this.modeManager = modeManager;
   this.element = element;
 }
+
+UploadMode.prototype.handleUploadStream = function(stream) {
+
+  var mesh = window.mesh = editor.createMesh();
+  var mode = this;
+
+  stream.on('data', function(obj) {
+    console.log('add', obj);
+    mesh.addFace(obj.verts, obj.normal);
+  });
+
+  stream.once('end', function() {
+    mesh.finalize();
+
+    // TODO: compute this over the next X seconds, possibly in a webworker
+    tools.computeNgonHelpers(mesh);
+
+    editor.addMesh(mesh);
+
+    // let the wiring take care of where we go from here.
+    mode.exit(mesh);
+  });
+};
 
 UploadMode.prototype.activate = function(last, editor) {
 
@@ -13,28 +38,17 @@ UploadMode.prototype.activate = function(last, editor) {
 
   this.element.parentElement.style.display = "block"
 
-  var dropper = dropHandler(this.element).once('stream', function(stl) {
+  var stream = postMessageImport();
+  if (stream) {
+    mode.handleUploadStream(stream);
+  } else {
 
-    var mesh = window.mesh = editor.createMesh();
-
-    stl.once('data', function() {
-      stl.on('data', function(obj) {
-        mesh.addFace(obj.verts, obj.normal);
+    var dropper = dropHandler(this.element).once('stream', function(stl) {
+      stl.once('data', function() {
+        mode.handleUploadStream(stl);
       });
     });
-
-    stl.once('end', function() {
-      mesh.finalize();
-
-      // TODO: compute this over the next X seconds, possibly in a webworker
-      tools.computeNgonHelpers(mesh);
-
-      editor.addMesh(mesh);
-
-      // let the wiring take care of where we go from here.
-      mode.exit(mesh);
-    });
-  });
+  }
 };
 
 UploadMode.prototype.deactivate = function(event) {
