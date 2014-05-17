ec3e72ffeb57358c53a05692d8fc894b0d3c2e25
Elijah Insua <tmpvar@gmail.com>
2014-05-17 20:55:33 +1000

    rewrite toolpath mode to hand changing params

--- modes/toolpath.js
+++ modes/toolpath.js
@@ -1,15 +1,129 @@
 var workerstream = require('workerstream');
 var Vec2 = require('vec2');
+var tincture = require('tincture');
+
 
 function ToolpathMode(editor) {
   this.editor = editor;
   var worker = workerify './../lib/toolpath-worker.js';
   this.worker = workerstream(worker);
 
-  window.worker=  this.worker;
+  var gcode = function(op, obj) {
+    return op + ' ' +  Object.keys(obj).map(function(n) {
+      var nl = n.toLowerCase();
+      if (nl !== 'x' && nl !== 'y' && nl !== 'z' && nl !== 'f') {
+        return;
+      }
+
+      return n.toUpperCase() + Number(obj[n]).toFixed(3);
+    }).filter(Boolean).join(' ');
+  };
+
+
+  var lineMaterial = new THREE.LineBasicMaterial({
+    color: 0xFF9D40,
+    opacity: 1,
+    linewidth: 2
+  });
+
+  this.worker.on('error', function(err) {
+    console.error(err);
+  });
+
+  var mode = this;
+  this.worker.on('data', function(data) {
+    if (data.name === 'log') {
+      console.log(data.data);
+    } else if (data.name === 'layer') {
+
+      if (!data.data.hulls) {
+        return;
+      }
+
+      var layer = [];
+
+      var z = data.data.z/mode.modelScale;
+      var hulls = data.data.hulls, l = hulls.length, lastGeometry;
+
+      for (var i=0; i<hulls.length; i++) {
+        var hullArray = hulls[i];
+
+        if (hullArray && hullArray.length) {
+          for (var j = 0; j<hullArray.length; j++) {
+
+            var r = hullArray[j];
+            if (r.length) {
+
+              var lineGeometry = new THREE.Geometry();
+              for (var k = 0; k < r.length; k++) {
+
+                r[k].X /= mode.modelScale;
+                r[k].Y /= mode.modelScale;
+
+                mode.gcode.push(gcode('G1', r[k]));
+
+                if (k === 0) {
+                  mode.gcode.push(gcode('G1', {
+                    z : -(mode.boundZ - z)
+                  }));
+                }
+
+                lineGeometry.vertices.push(
+                  new THREE.Vector3(r[k].X, r[k].Y, z)
+                );
+              }
+
+              mode.gcode.push(gcode('G1', r[0]));
+              mode.gcode.push(gcode('G1', { z : 5 }));
+
+              lineGeometry.vertices.push(
+                new THREE.Vector3(r[0].X, r[0].Y, z)
+              );
+
+              mode.toolPathDisplay.add(new THREE.Line(
+                lineGeometry,
+                lineMaterial,
+                THREE.LineStrip
+              ));
+            }
+          }
+        }
+      }
+
+      mode.gcode.push(gcode('G1', { z : 5 }));
+
+    } else if (data.name === 'grind') {
+      console.log('elapsed', (Date.now() - mode.grindStart) + 'ms');
+    }
+  });
+
   this.layers = [];
 
+  this.form = tincture(
+    editor.parentElement.querySelector('#toolpath-dialog')
+  );
+
+  var that = this, throttleTimer;
+  var handlePropertyChange = function(name, current, old) {
+    if (current === old) {
+      return;
+    }
+
+    if (throttleTimer) {
+      clearTimeout(throttleTimer);
+    }
+
+    throttleTimer = setTimeout(mode.generate.bind(mode), 200);
+  };
+
+  setTimeout(function() {
+    mode.form.depthPerPass.change(handlePropertyChange.bind(null, 'depthPerPass'));
+    mode.form.scale.change(handlePropertyChange.bind(null, 'scale'));
+    mode.form.toolDiameter.change(handlePropertyChange.bind(null, 'toolDiameter'));
+  }, 200);
 }
+
+ToolpathMode.prototype.modelScale = 1000;
 
 ToolpathMode.prototype.deactivate = function(last) {
   Array.prototype.push.apply(this.gcode, [
@@ -23,139 +137,13 @@ ToolpathMode.prototype.deactivate = function(last) {
 };
 
 ToolpathMode.prototype.activate = function(last, mesh) {
-  this.gcode = [
-    '$H',
-    'M4 S' + 7000,
-    'G4 P5',
-    'G10 L20 P1 X0 Y0 Z0',
-    'G21G17',
-    'G1 X' + (this.startX || 10) + ' Y' + (this.startY || 10) + ' F4000',
-    'G1 Z' + (this.startZ || -5),
-    'G10 L20 P1 X0 Y0 Z0',
-    'G28.1 X0 Y0 Z0',
-    'G1 Z5'
-  ];
 
   var h1 = this.editor.parentElement.querySelector('h1');
   h1.innerHTML = "let's generate toolpaths";
   this.mesh = mesh;
-
-  // TODO: interface for changing
-  //  * depth per cut
-  //  * tool diameter
-  var editor = this.editor;
-  var mode = this;
-  var modelScale = 100;
-
-  var lineMaterial = new THREE.LineBasicMaterial({
-    color: 0xFF9D40,
-    opacity: 1,
-    linewidth: 2
-  });
-
-  this.worker.on('error', function(err) {
-    console.error(err);
-  });
-
-
-  var gcode = function(op, obj) {
-    return op + ' ' +  Object.keys(obj).map(function(n) {
-      if (n !== 'x' && n !== 'y' && n !== 'z' && n !== 'f') {
-        return;
-      }
-
-      return n.toUpperCase() + Number(obj[n]).toFixed(2);
-    }).filter(Boolean).join(' ');
-  };
-
-  var bb = mesh.boundingBox();
-  var boundZ = bb[1][2] - bb[0][2];
-
-  this.worker.on('data', function(data) {
-    if (data.name === 'layer') {
-
-      if (!data.data.hulls) {
-        return;
-      }
-
-      var layer = [];
-
-      var z = data.data.z/modelScale;
-      var hulls = data.data.hulls, l = hulls.length, lastGeometry;
-
-
-      for (var i=0; i<hulls.length; i++) {
-        var points = [];
-        var hullArray = hulls[i];
-
-        if (hullArray && hullArray.length) {
-          for (var j = 0; j<hullArray.length; j++) {
-
-            var r = hullArray[j];
-            if (r.length) {
-
-              var lineGeometry = new THREE.Geometry();
-              for (var k = 0; k < r.length; k++) {
-
-                var v = new Vec2(r[k].X/modelScale, r[k].Y/modelScale);
-
-                points.push(v);
-                mode.gcode.push(gcode('G1', v));
-
-                if (k === 0) {
-                  mode.gcode.push(gcode('G1', {
-                    z : -(boundZ - z)
-                  }));
-                }
-
-                lineGeometry.vertices.push(
-                  new THREE.Vector3(v.x, v.y, z)
-                );
-              }
-
-              var v = new Vec2(r[0].X/modelScale, r[0].Y/modelScale);
-
-              mode.gcode.push(gcode('G1', v));
-
-              mode.gcode.push(gcode('G1', { z : 5 }));
-
-              lineGeometry.vertices.push(
-                new THREE.Vector3(
-                  v.x,
-                  v.y,
-                  z
-                )
-              );
-
-              editor.scene.add(new THREE.Line(
-                lineGeometry,
-                lineMaterial,
-                THREE.LineStrip
-              ));
-            }
-          }
-        }
-      }
-
-      mode.gcode.push(gcode('G1', { z : 5 }));
-
-    } else if (data.name === 'grind') {
-      console.log('elapsed', (Date.now() - start) + 'ms');
-    }
-  });
+  var modelScale = this.modelScale;
 
   var geometry = mesh.geometry;
-
-  this.worker.write({
-    name : 'configure',
-    data : {
-      toolDiameter: 2.5*modelScale,
-      // Dodge a slight issue with mesh-slice-polygon
-      stocktop: bb[1][2]*modelScale,
-      depth: (bb[1][2]*modelScale)/5
-    }
-  });
-
   var verts = geometry.vertices;
   var mx = mesh.position.x;
   var my = mesh.position.y;
@@ -174,7 +162,54 @@ ToolpathMode.prototype.activate = function(last, mesh) {
     });
   }
 
-  var start = Date.now();
+  this.generate();
+}
+
+ToolpathMode.prototype.generate = function() {
+
+  if (this.toolPathDisplay) {
+    this.editor.scene.remove(this.toolPathDisplay);
+  }
+
+  if (!this.mesh || !this.form.depthPerPass()) {
+    return;
+  }
+
+  this.toolPathDisplay = new THREE.Object3D();
+  this.editor.scene.add(this.toolPathDisplay);
+
+  this.gcode = [
+    '$H',
+    'M4 S' + 7000,
+    'G4 P5',
+    'G10 L20 P1 X0 Y0 Z0',
+    'G21G17',
+    'G1 X' + (this.startX || 10) + ' Y' + (this.startY || 10) + ' F4000',
+    'G1 Z' + (this.startZ || -5),
+    'G10 L20 P1 X0 Y0 Z0',
+    'G28.1 X0 Y0 Z0',
+    'G1 Z5'
+  ];
+
+  var editor = this.editor;
+  var mode = this;
+  var modelScale = this.modelScale;
+
+  var bb = mesh.boundingBox({ x: 0, y: 0, z: 0});
+
+  this.boundZ = bb[1][2] - bb[0][2];
+  var posZ = mesh.position.z;
+
+  this.worker.write({
+    name : 'configure',
+    data : {
+      toolDiameter: this.form.toolDiameter()*modelScale,
+      stockTop: (posZ + (bb[1][2] - bb[0][2])/2)*modelScale,
+      stockBottom: (posZ + bb[0][2])*modelScale,
+      depthPerPass: modelScale*this.form.depthPerPass()
+    }
+  });
+  this.grindStart = Date.now();
   this.worker.write({ name : 'grind' });
 };
 
